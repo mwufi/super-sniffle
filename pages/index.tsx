0e357d529151f1e39b924105a022f9508fbe13be
Zen <ztang230@gmail.com>
2022-03-15 22:55:20 -0500

    eh, idk

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,14 +32,62 @@ const credits = (
   </Slide>
 )
 
+const quote = (
+  <Slide left dark>
+    <Text>
+      Challenge: make it so that <GradientText>anyone</GradientText> can
+      contribute to solving a city's problems
+    </Text>
+  </Slide>
+)
+
+const intro = (
+  <Slide>
+    <Heading>
+      <GradientText>Hi, I'm Zen</GradientText>
+    </Heading>
+    <Text>
+      Thinking a lot lately about <GradientText>government</GradientText>, and
+      how it works, and how to make it better.
+    </Text>
+    <Text>(like this style? Grab it on Github)</Text>
+  </Slide>
+)
+const work = (
+  <Slide dark>
+    <Heading>Work Experience</Heading>
+    <Heading>
+      <GradientText>Wayve</GradientText>
+    </Heading>
+    <SubHeading>Self driving cars</SubHeading>
+    <Text>
+      2019 - 2020. I used to be really interested in self-driving, and AI! Out
+      of the blue, I emailed the founder, after coming across his paper -- and
+      moved to London!
+    </Text>
+    <div className="pt-4"></div>
+    <Heading>
+      <GradientText>TikTok</GradientText>
+    </Heading>
+    <SubHeading>Recommendations team</SubHeading>
+    <Text>
+      Recommendations is interesting. Surprisingly, a lot of it turns out to be
+      about the ecosystem, and how to improve it!
+    </Text>
+    <Text>
+      Come join us! If you're interested in what happens "behind the scenes", or
+      in working in a cool org, this is a great place to be!
+    </Text>
+  </Slide>
+)
+
 const Home: NextPage = () => {
   return (
-    <div className="space-y-0.5 bg-orange-200 p-0.5">
+    <div className="space-y-1 bg-orange-200 p-1 md:p-3">
       <Head>
         <title>Allo - Government as a Service</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Slide center>
         <Heading>AlloGov</Heading>
         <SubHeading>
@@ -49,79 +97,106 @@ const Home: NextPage = () => {
       </Slide>
 
       <Slide dark left>
+        <div className="uppercase">problem</div>
         <SubHeading>
-          Cities today are <GradientText>limited</GradientText>
+          Governments are <GradientText>invisible</GradientText>
         </SubHeading>
         <Text>
           <li>Huge challenges, like homelessness or fiscal shortfalls</li>
-          <li>Traditional tools like contracts can't solve that</li>
-          <li>
-            Government needs innovation{' '}
-            <GradientText>from the outside</GradientText>
-          </li>
-        </Text>
-      </Slide>
-
-      <Slide left dark>
-        <Text>
-          Challenge: make it so that <GradientText>anyone</GradientText> can
-          contribute to solving a city's problems
+          <li>Most people don't care</li>
+          <li>Good decisions are rarely recognized by the public</li>
+          <li>Why? Bad UX.</li>
         </Text>
       </Slide>
 
       <Slide>
+        <div className="uppercase">solution</div>
+
         <Heading>
-          Solution: <br />
-          Bring cities <GradientText>online.</GradientText>
+          an app that <br />
+          <GradientText>connects you</GradientText> <br />
+          in every city
         </Heading>
         <SubHeading>
-          <Text>An app in everyone's pocket</Text>
+          <Text>Fun to use</Text>
           <Text>
-            We do the hard work of government integration, from identity to
-            services to processes
+            Relevant, social, useful...{' '}
+            <GradientText>and also used by government</GradientText>
           </Text>
           <Text>Result: New types of experiences.</Text>
         </SubHeading>
       </Slide>
+
       <Slide>
-        <Heading>
-          Easy. Helpful. <GradientText>CityPass.</GradientText>
-        </Heading>
         <SubHeading>
-          <Text>Digital services, courtesy of the city</Text>
-          <Text>
-            A <GradientText>cash voucher app</GradientText> that allows
-            municipal governments to budget up to $10 per citizen, per month.
-          </Text>
-          <Text>Forums, local events, sign in to the library, etc.</Text>
+          Ubiquitous. <GradientText>CityPass.</GradientText>
         </SubHeading>
+        <SubHeading>
+          <Text>Local events, discussions</Text>
+          <Text>
+            A <GradientText>cash account</GradientText> that allows municipal
+            governments to budget up to $10 per citizen, per month.
+          </Text>
+          <Text>
+            A secure <GradientText>extensible identity</GradientText>
+          </Text>
+        </SubHeading>
+        <div>or, Metamask meets WeChat?</div>
+      </Slide>
+
+      <Slide dark>
+        <div className="uppercase">market size</div>
+        <SubHeading>Govtech is a growing market</SubHeading>
+        <Text>Rough estimate:</Text>
+        <Text>
+          <GradientText>subscriptions</GradientText> - $600M/year - $5/mo * 10M
+          people{' '}
+        </Text>
+        <Text>
+          <GradientText>payments</GradientText> - $100M/year - 1% * 100M people
+          * $100/year{' '}
+        </Text>
+        <Text>
+          <GradientText>software</GradientText> - $600M/year - $5000/mo * 10k
+          cities{' '}
+        </Text>
       </Slide>
 
       <Slide>
-        <Heading>For governments</Heading>
+        <SubHeading>Adoption Strategy</SubHeading>
         <Text>
-          Replace complicated processes. Offer world-class experiences.
+          Direct to municipality - working with city managers to trial the
+          program
         </Text>
-        <Text>
-          Ship <GradientText>open APIs</GradientText> for young innovators and
-          developers
-        </Text>
-        <Text>Save on software (and more), and let the people know</Text>
-        <Text>
-          Get <GradientText>more insight</GradientText> from citizens
-        </Text>
+        <Text>Launching by city</Text>
       </Slide>
 
       <Slide>
-        <SubHeading>Huge market</SubHeading>
-        <SubHeading>$400B + $XXB + $XXB</SubHeading>
-        <Text>Combination of govtech, crypto, and consumer</Text>
-        <Text><GradientText>premium subscriptions</GradientText> - $600M/year - $5/mo * 10M people </Text>
-        <Text><GradientText>payments</GradientText> - $100M/year - 1% * 100M people * $100/year </Text>
-        <Text><GradientText>government as the customer</GradientText> - $600M/year - $5000/mo * 10k cities </Text>
+        <SubHeading>Team</SubHeading>
+        <div className="flex gap-8">
+          <img
+            className="w-48 h-48 object-cover rounded-xl"
+            src="https://media-exp1.licdn.com/dms/image/C5603AQFEXJ5-mjkhPA/profile-displayphoto-shrink_200_200/0/1595178999211?e=1652313600&v=beta&t=c3W4SSbe8SOtwK5rkMyKckt6egn7eyRG0VtNtHDuEI8"
+          />
+          <Text>
+            Zen Tang - <GradientText>Tech</GradientText>.
+          </Text>
+        </div>
+        <div className="flex gap-8">
+          <img
+            className="w-48 h-48 object-cover rounded-xl"
+            src="https://media.istockphoto.com/photos/silhouette-of-asian-high-school-girl-picture-id1306611068?b=1&k=20&m=1306611068&s=170667a&w=0&h=ZAPXPzGTN2XEc5zhVxwPttICWx7SeRsvozyuN1StoLQ="
+          />
+          <Text>
+            Unknown - <GradientText>Co-founder?</GradientText>
+          </Text>
+        </div>
       </Slide>
+
       <Slide>
-        <SubHeading>Competition</SubHeading>
+        <SubHeading>Milestones</SubHeading>
+        <Text>May - June: Talk to government customers</Text>
+        <Text>June - Aug: Build MVP</Text>
       </Slide>
     </div>
   )
